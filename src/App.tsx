1266f4bbd6b4236995389608cfaa698d8f40bc87
Jack Sheehy <jackhughsheehy@gmail.com>
2023-11-29 03:14:46 +0000

    Font / UI Change

--- src/App.tsx
+++ src/App.tsx
@@ -22,13 +22,13 @@ function App() {
       <div className="font-josefin flex min-h-screen select-none bg-[#F5F5F5]">
         {/* Left side - Hidden on small screens */}
         <div className="hidden md:flex md:flex-1 md:flex-col md:items-center md:justify-center">
-          <h1 className="px-4 pb-6 pt-48 text-center text-7xl font-bold text-[black]">
-            A Real-Time <span className="  text-[5rem] text-[#ed0202]">SNKRS</span> Monitor, for free.
+          <h1 className="px-4 pb-6 pt-48 text-center text-8xl font-bold text-[black]">
+            A Real-Time <span className="  text-8xl text-[#c70b14]">SNKRS</span> Monitor, for free.
           </h1>
           <img className="pointer-events-none select-none" src={Jordans} width={500} height={500} />
         </div>
         {/* Right side */}
-        <main className="flex flex-1 flex-col items-center justify-start bg-[#ed0202] px-3">
+        <main className="flex flex-1 flex-col items-center justify-start bg-[#c70b14] px-3">
           {/* Content for both small and large screens */}
           <div className="flex gap-4 pt-10">
             <a href="https://www.reddit.com/r/SNKRS" target="_blank" rel="noopener noreferrer">
@@ -46,17 +46,18 @@ function App() {
               />
             </a>
           </div>
-          <h1 className="font-Josefin-Sans pb-6 pt-36 text-center text-8xl font-bold text-white sm:text-left">Coming Soon</h1>
+          <h1 className="font-Josefin-Sans pb-0 pt-36 text-center text-8xl font-bold text-white sm:text-left">Coming Soon</h1>
 
-          <h2 className="max-w-2xl  text-center text-xl text-white opacity-80">
+          <h2 className="max-w-md  text-center text-xl text-white opacity-80">
             Never miss a restock. Real-time monitoring of SNKRS drops. Get instant updates on all kinds of drops over email or text, for free.
           </h2>
-          <div className="flex w-full max-w-md items-center space-x-2 pt-8">
-            <input className="h-10 w-64 rounded-md border-0 bg-zinc-900 px-2 py-1 text-white placeholder-zinc-400 " type="email" placeholder="Email " />
-            <button className="py-1.5 w-40  border-2 rounded-md border-white bg-white text-black" type="submit">
+          <div className="flex flex-col items-center space-y-2 pt-12">
+            <input className="h-10 w-64 rounded-md border-0 bg-zinc-900 px-2 py-1 text-white placeholder-zinc-400" type="email" placeholder="Email" />
+            <button className="py-1.5 w-40 border-2 rounded-md border-white bg-white text-black" type="submit">
               Join the waitlist
             </button>
           </div>
+
           <footer className="fixed bottom-0 p-4 text-2xl text-center text-white">
             <a href="mailto:snkrs.reddit@gmail.com">
               <IoMdMail />
